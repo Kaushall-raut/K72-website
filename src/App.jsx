9544b589df0a2579072f706382a9d901d8975468
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-08-22 22:34:22 +0530

    routing add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
-
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Agents from "./pages/Agents";
 
 function App() {
+  return (<>
   
-
-  return (
-  <div className="text-red-800 text-4xl">Hello world</div>
-  )
+      <Link to={"/"}>Home</Link>
+      <Link to={"/Agents"}>Agents</Link>
+      <Link to={"/projects"}>project</Link>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Agents" element={<Agents />} />
+      <Route path="/Projects" element={<Projects />} />
+    </Routes>
+  </>
+  );
 }
 
-export default App
+export default App;
