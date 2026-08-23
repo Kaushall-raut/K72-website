df74b3bd993d1cefe115d98400ef2cb6ebab01bb
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-08-23 09:56:27 +0530

    minor css changes

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
   return (
-    <div className="flex items-center justify-center gap-2 font-[font2]">
-      <Link className="uppercase text-[6.8vw] border-[2.5px] rounded-full leading-[5.5vw] px-10 pt-4.5 mb-2 font-bold  ">Work</Link>
-      <Link className="uppercase text-[6.8vw] border-[2.5px] rounded-full leading-[5.5vw] px-10 pt-4.5 mb-2 font-bold">Agency</Link>
+    <div className="flex items-center justify-center uppercase gap-2 overflow-hidden font-[font2]">
+      <div className="border-3 hover:text-[#D3FD50] h-25  mb-2  flex items-center rounded-full px-8    ">
+        <Link to={"agents"} className=" text-[7vw] mt-4  font-bold ">
+          Work
+        </Link>
+      </div>
+      <div className="border-3  hover:text-[#D3FD50] h-25  mb-2 rounded-full flex items-center px-8 ">
+        <Link to={"agents"} className=" text-[7vw] mt-4 font-bold ">
+          Agency
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBottomText
+export default HomeBottomText;
