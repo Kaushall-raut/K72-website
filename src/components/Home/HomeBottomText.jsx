eadc9a6af4d9bc9af0a2edb7e42d483270e6bdc0
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-08-23 09:16:31 +0530

    home page bottom  text add

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
 
 
 const HomeBottomText = () => {
   return (
-    <div>
-      HomeBottomText
+    <div className="flex items-center justify-center gap-2 font-[font2]">
+      <Link className="uppercase text-[6.8vw] border-[2.5px] rounded-full leading-[5.5vw] px-10 pt-4.5 mb-2 font-bold  ">Work</Link>
+      <Link className="uppercase text-[6.8vw] border-[2.5px] rounded-full leading-[5.5vw] px-10 pt-4.5 mb-2 font-bold">Agency</Link>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import HomeBottomText from "../components/Home/HomeBottomText";
 import HomeHeroText from "../components/Home/HomeHeroText";
 import Video from "../components/Home/Video";
 
@@ -7,8 +8,9 @@ const Home = () => {
       <div className="h-screen w-screen fixed">
         <Video />
       </div>
-      <div className="h-screen w-screen flex flex-col relative">
-        <HomeHeroText/>
+      <div className="h-screen justify-between w-screen flex flex-col relative">
+        <HomeHeroText />
+        <HomeBottomText />
       </div>
     </div>
   );
