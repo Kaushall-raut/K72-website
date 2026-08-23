133c7087a951d61cc135e670974d35d632565987
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-08-23 08:41:07 +0530

    home page add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-
+import HomeHeroText from "../components/Home/HomeHeroText";
+import Video from "../components/Home/Video";
 
 const Home = () => {
   return (
     <div>
-      home
+      <div className="h-screen w-screen fixed">
+        <Video />
+      </div>
+      <div className="h-screen w-screen flex flex-col relative">
+        <HomeHeroText/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
