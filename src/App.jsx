950f16adfb66d973c68134fe1d30bc1145b9c952
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-08-31 20:39:10 +0530

    css worked on navbar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Agents from "./pages/Agents";
-import NavBar from "./components/Common/NavBar";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import Agents from "./pages/Agents";
+// import NavBar from "./components/Common/NavBar";
+
+import FullScreenNav from "./components/Common/FullScreenNav";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <FullScreenNav/>
+      {/* <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Agents" element={<Agents />} />
         <Route path="/Projects" element={<Projects />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- src/components/Common/FullScreenNav.jsx
+++ src/components/Common/FullScreenNav.jsx
@@ -1,15 +1,27 @@
 const FullScreenNav = () => {
   return (
-    <div>
+    <div className="h-screen text-white w-full absolute py-32 bg-amber-400">
       <div>
-        <div>
-          <h1>Projects</h1>
-          <div>
-            <div>
-              <h2>SEE EVERYTHING </h2>
-              <img src="public\images\PJC_SiteK72_Thumbnail_640x290.jpg" />
-              <h2>SEE EVERYTHING </h2>
-              <img src="public\images\Thumbnail.png" />
+        <div className="link border-t relative">
+          <h1 className="font-[font2] text-[8vw] text-center pt-10 leading-[0.8] uppercase ">
+           work
+          </h1>
+          <div className="absolute flex top-0 text-black bg-[#D3FD50]">
+            <div className="flex items-center overflow-x-auto">
+              <h2 className="whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8]">
+                SEE EVERYTHING{" "}
+              </h2>
+              <img
+                className="h-24 w-72 rounded-full object-cover  shrink-0"
+                src="public\images\PJC_SiteK72_Thumbnail_640x290.jpg"
+              />
+              <h2 className="whitespace-nowrap  font-[font2] text-[8vw] text-center leading-[0.8]">
+                SEE EVERYTHING{" "}
+              </h2>
+              <img
+                className="h-36 w-96 rounded-full object-cover shrink-0 "
+                src="public\images\Thumbnail.png"
+              />
             </div>
           </div>
         </div>
